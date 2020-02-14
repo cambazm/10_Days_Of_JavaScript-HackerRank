3555b6b3eadbe9b403ec32ac8b2f1d888cdb2043
cambazm <56502154+cambazm@users.noreply.github.com>
2020-02-14 13:29:19 +0400

    Update Let and Const.js
    in order to calculate a float result, we need to get input as float (decimal)

--- Day 1/Let and Const.js
+++ Day 1/Let and Const.js
@@ -1,7 +1,7 @@
 function main() {
     // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
     const PI = Math.PI;
-    var r = parseInt(readLine());
+    var r = parseFloat(readLine());
     // Print the area of the circle:
     var area = PI * r * r;
     console.log(area);
